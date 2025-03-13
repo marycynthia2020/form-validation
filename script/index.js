66cd032beb38a0748a34725a6551e60c616a858c
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-13 14:17:43 +0100

    implements form and validation

---
+++ script/index.js
@@ -0,0 +1,66 @@
+const registerBtn = document.getElementById("submit");
+
+registerBtn.addEventListener("click", register);
+
+function register(e) {
+  e.preventDefault();
+
+  const Fname = document.getElementById("Fname").value;
+  const Lname = document.getElementById("Lname").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+  const phoneNumber = document.getElementById("number").value;
+  let gender;
+
+  if (document.getElementById("male").checked) {
+    gender = document.getElementById("male").value;
+  } else if (document.getElementById("female").checked) {
+    gender = document.getElementById("female").value;
+  }
+
+  if (
+    Fname == "" ||
+    Lname == "" ||
+    email == "" ||
+    address == "" ||
+    phoneNumber == "" ||
+    !gender
+  ) {
+    document.getElementById("error").textContent = "All fields are required";
+  }
+  if (Fname == "") {
+    document.getElementById("Fname-error").textContent = "required";
+  }
+
+  if (Lname == "") {
+    document.getElementById("Lname-error").textContent = "required";
+  }
+
+  if (email == "") {
+    document.getElementById("email-error").textContent = "required";
+  }
+
+  if (address == "") {
+    document.getElementById("address-error").textContent = "required";
+  }
+
+  if (phoneNumber == "") {
+    document.getElementById("phone-error").textContent = "required";
+  }
+
+  if (!gender) {
+    document.getElementById("gender-error").textContent = "required";
+  } else {
+    document.getElementById("heading").textContent =
+      "Your Registration is Successfull";
+    document.getElementById("error").textContent = "";
+    let item = document.getElementsByTagName("input");
+    for (let i = 0; i < item.length; i++) {
+      console.log((item[i].value = ""));
+    }
+  }
+}
+
+function clearField(selector) {
+  document.getElementById(selector).textContent = "";
+}
